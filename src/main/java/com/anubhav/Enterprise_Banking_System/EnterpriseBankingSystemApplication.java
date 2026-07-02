4ee7a7dc2b3dd21e5f926c737002ccc470ca0b55
Anubhav Kumar <anubhavkumar2607@gmail.com>
2026-07-02 19:19:45 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/anubhav/Enterprise_Banking_System/EnterpriseBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.anubhav.Enterprise_Banking_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseBankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anubhav/Enterprise_Banking_System/EnterpriseBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anubhav.Enterprise_Banking_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
